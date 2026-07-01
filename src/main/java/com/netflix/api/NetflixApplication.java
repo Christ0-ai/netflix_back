647d89e4fd8ee23f67f1eaf29b63ead623658078
murciana.le.bouedec <murciana.le.bouedec@accenture.com>
2026-07-01 16:01:01 +0200

    first commit

---
+++ src/main/java/com/netflix/api/NetflixApplication.java
@@ -0,0 +1,13 @@
+package com.netflix.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(NetflixApplication.class, args);
+    }
+
+}
